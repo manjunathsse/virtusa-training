1a8d8d8c748f01bea6593731408a7809bce93773
Manjunatha reddy Lingamdinne <88681651+manjunathsse@users.noreply.github.com>
2022-12-03 12:17:27 +0530

    Add files via upload

---
+++ CheckArrays.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CheckArrays {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number of array elements");
+		int n=s.nextInt();
+		System.out.println("Enter the array elements");
+		
+	}
+}
